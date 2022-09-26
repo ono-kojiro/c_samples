8ca281ffe24df037f94d2ee573a563c9770aecc1
Kojiro ONO <ono.kojiro@gmail.com>
2022-09-26 21:15:51 +0900

    switch mylib

--- package/myapp/src/myapp.c
+++ package/myapp/src/myapp.c
@@ -1,10 +1,23 @@
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#if HAVE_MYLIB
+#include "mylib.h"
+#endif
 
 int main(int argc, char **argv)
 {
 	printf("This is myapp.\n");
 	printf("add stdlib.h\n");
+
+#if HAVE_MYLIB
+	mylib();
+#else
+	printf("mylib() is NOT called.\n");
+#endif
+
 	return 0;
 }
 
