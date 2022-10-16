b48e86144389b2cc24468c8cb1dfdb8afdd5b3ce
Kojiro ONO <ono.kojiro@gmail.com>
2022-10-16 19:06:39 +0900

    add testcase of TIMES

--- calc/tests/01-mycalc_test.sh
+++ calc/tests/01-mycalc_test.sh
@@ -8,7 +8,7 @@ cd $top_dir
 basename=$(basename $0 .sh)
 MYCALC=../mycalc/mycalc
 
-echo 1..2
+echo 1..3
 res=$(echo "1+2" | $MYCALC)
 if [ "$res" = "3" ]; then
   echo "ok - 1+2=$res"
@@ -21,6 +21,13 @@ if [ "$res" = "7" ]; then
   echo "ok - 3+4=$res"
 else
   echo "not ok - 3+4=$res"
+fi
+
+res=$(echo "5+6*7" | $MYCALC)
+if [ "$res" = "47" ]; then
+  echo "ok - 5+6*7=$res"
+else
+  echo "not ok - 5+6*7=$res"
 fi
 
 cd $oldpwd
