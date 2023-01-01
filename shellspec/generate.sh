8ea2db4d72dbd3d0e41b8e035153646290e00b9e
Kojiro ONO <ono.kojiro@gmail.com>
2023-01-01 17:53:18 +0900

    remove debug print

--- shellspec/generate.sh
+++ shellspec/generate.sh
@@ -3,10 +3,7 @@
 scripts=$(find ./scripts/ -name "*.sh")
 
 for filepath in $scripts; do
-  echo "This is $filepath"
-
   basename=$(basename $filepath .sh)
-  echo basename is $basename
   testspec="spec/${basename}_spec.sh"
 
   cat - << EOS > $testspec
