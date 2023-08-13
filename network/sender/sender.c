027cfe30946d6268ec082ad858347b8dac14ad5d
Kojiro ONO <ono.kojiro@gmail.com>
2023-08-13 22:05:19 +0900

    multicast passed

--- network/sender/sender.c
+++ network/sender/sender.c
@@ -10,23 +10,25 @@
 
 #include <getopt.h>
 
-#include "common.h"
-#include "recv_handler.h"
+//#include "common.h"
+//#include "recv_handler.h"
 
 struct userdata {
 	int soc;
 	const char *host;
 	const char *port;
-	const char *multicast_address;
+	const char *multicast_addr;
+	struct addrinfo *info;
 };
 
 void sig_int_handler(int fd, short event, void *arg)
 {
+    struct event_base *base = (struct event_base *)arg;
 	fprintf(stdout, "\n");
-	event_loopexit(NULL);
+	event_base_loopexit(base, NULL);
 }
 
-void read_stdin(int fd, short event, void *arg)
+void stdin_handler(int fd, short event, void *arg)
 {
 	int ret;
 
@@ -35,6 +37,7 @@ void read_stdin(int fd, short event, void *arg)
 	const char *host = data->host;
 	const char *port = data->port;
 	const char *multicast_address = data->multicast_address;
+    struct addrinfo *info = data->info;
 	
 	char buf[256];
 	int len;
@@ -44,51 +47,133 @@ void read_stdin(int fd, short event, void *arg)
 	struct sockaddr_storage dst;
 	socklen_t dst_len;
 
-	//fprintf(stderr, "read_stdin called\n");
-
 	if(event & EV_READ){
 		len = read(fd, buf, sizeof(buf) - 1);
 		if(len == -1){
 			perror("read error");
 		}
 		else if(len == 0){
-			perror("closed");
+			// Ctrl+D
 		}
 		else{
-			if(buf[len - 1] == '\n'){
-				buf[len - 1] = '\0';
-			}
-			else{
-				buf[len] = '\0';
-			}
-
-			fprintf(stderr, "[send] : '%s'\n", buf);
-			fprintf(stderr, "[dest] %s:%s\n", multicast_address, port);
-
-			err = get_sockaddr_info(multicast_address, port, &dst, &dst_len);
-			if(err == -1){
-				perror("get_sockaddr_info");
-				event_loopexit(NULL);
-			}
-	
-			ret = sendto(soc, buf, strlen(buf), 0,
-					(struct sockaddr *)&dst, dst_len);
+			ret = sendto(soc, buf, len, 0,
+					info->ai_addr, info->ai_addrlen);
 			if(ret == -1){
 				perror("sendto");
 				event_loopexit(NULL);
 			}
 	
-			if(!strcmp(buf, "exit")){
-				//fprintf(stderr, "exit read_stdin\n");
-				event_loopexit(NULL);
-			}
-			else {
-				fprintf(stdout, "> ");
-			}
+			fprintf(stdout, "> ");
 		}
-		//event_add(ev, NULL);
 	}
 }
+
+int get_addr_info(const char *host, const char *port,
+            struct addrinfo **info)
+{
+    int err;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    err = getaddrinfo(host, port, &hints, info);
+    if(err != 0){
+        fprintf(stderr, "getaddrinfo():%s\n", gai_strerror(err));
+        return -1;
+    }
+
+    return 0;
+}
+
+int print_name_info(struct addrinfo *info)
+{
+    int err;
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    err = getnameinfo(
+            info->ai_addr, info->ai_addrlen,
+            host,    NI_MAXHOST,
+            service, NI_MAXSERV,
+            NI_NUMERICHOST | NI_NUMERICSERV);
+
+    if(err != 0){
+        fprintf(stderr, "getnameinfo():%s\n", gai_strerror(err));
+        exit(1);
+    }
+
+    fprintf(stderr, "host=%s, service=%s\n", host, service);
+
+    return 0;
+}
+
+int sender_socket(const char *host, const char *port, 
+				const char *multicast_addr)
+{
+	int soc;
+
+	int err;
+	struct addrinfo *info;
+
+	unsigned char multicast_ttl = 64;
+	unsigned char multicast_loopback = 1;
+
+	soc = socket(PF_INET, SOCK_DGRAM, 0);
+	if(soc == -1){
+		perror("socket");
+		freeaddrinfo(info);
+		return -1;
+	}
+
+	{
+		struct addrinfo hints;
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_INET;
+		//hints.ai_socktype = SOCK_STREAM;
+		hints.ai_socktype = SOCK_DGRAM;
+
+		err = getaddrinfo(host, port, &hints, &info);
+		if(err != 0){
+			fprintf(stderr, "getaddrinfo():%s\n", gai_strerror(err));
+			return -1;
+		}
+	}
+
+	/* multicast settings */
+	err = setsockopt(soc, IPPROTO_IP, IP_MULTICAST_IF,
+					&((struct sockaddr_in *)info->ai_addr)->sin_addr,
+					sizeof(struct in_addr));
+	if(err == -1){
+		perror("setsockopt");
+		freeaddrinfo(info);
+		close(soc);
+		return -1;
+	}
+
+	err = setsockopt(soc, IPPROTO_IP, IP_MULTICAST_TTL,
+					&multicast_ttl, sizeof(multicast_ttl));
+	if(err == -1){
+		perror("setsockopt");
+		freeaddrinfo(info);
+		close(soc);
+		return -1;
+	}
+
+	err = setsockopt(soc, IPPROTO_IP, IP_MULTICAST_LOOP,
+					&multicast_loopback, sizeof(multicast_loopback));
+	if(err == -1){
+		perror("setsockopt");
+		freeaddrinfo(info);
+		close(soc);
+		return -1;
+	}
+	
+	freeaddrinfo(info);
+
+	return soc;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -96,9 +181,10 @@ int main(int argc, char **argv)
 
 	int soc;
 
-	struct event ev;
-	struct event sig;
-	struct event ev_io;
+    struct event_base *base;
+	struct event *ev_rcv;
+    struct event *ev_int;
+	struct event *ev_io;
 
 	struct option options[] = {
 		{ "host", required_argument, 0, 'h' },
@@ -110,7 +196,8 @@ int main(int argc, char **argv)
 	const char *host = NULL;
 	const char *port = NULL;
 
-	const char *multicast_address = NULL;
+	const char *multicast_addr = NULL;
+    struct addrinfo *info;
 
 	struct userdata data;
 
@@ -131,7 +218,7 @@ int main(int argc, char **argv)
 				port = optarg;
 				break;
 			case 'm' :
-				multicast_address = optarg;
+				multicast_addr = optarg;
 				break;
 			default :
 				break;
@@ -148,54 +235,59 @@ int main(int argc, char **argv)
 		ret++;
 	}
 
-	if(multicast_address == NULL){
+#if 0
+	if(multicast_addr == NULL){
 		fprintf(stderr, "no multicast-address option\n");
 		ret++;
 	}
+#endif
 
 	if(ret){
 		exit(1);
 	}
 
-	event_init();
-	signal_set(&sig, SIGINT, sig_int_handler, NULL);
-	signal_add(&sig, NULL);
+	base = event_base_new();
+	ev_int = evsignal_new(base, SIGINT, sig_int_handler, (void *)base);
+	signal_add(ev_int, NULL);
 
-	soc = sender_socket(host, port, multicast_address);
+    ret = get_addr_info(NULL, port, &info);
+    if(ret != 0){
+        exit(1);
+    }
+    print_name_info(info);
+
+    data.info = info;
+
+	soc = sender_socket(host, port, multicast_addr);
 	if(soc == -1){
 		fprintf(stderr, "sender_socket failed\n");
 		exit(1);
 	}
 
-#if 0
-	event_set(&ev, soc, EV_READ | EV_PERSIST, recv_handler, &ev);
-	err = event_add(&ev, NULL);
-	if(err != 0){
-		perror("event_add");
-		close(soc);
-		exit(1);
-	}
-#endif
-
 	data.soc = soc;
 	data.host = host;
 	data.port = port;
-	data.multicast_address = multicast_address;
-
+	data.multicast_addr = multicast_addr;
+	
 	setbuf(stdin, NULL);
 	setbuf(stdout, NULL);
-	event_set(&ev_io, 0 /* stdin */, EV_READ | EV_PERSIST,
-			read_stdin, (void *)&data);
-	err = event_add(&ev_io, NULL);
+	ev_io = event_new(base,
+                0 /* stdin */,
+                EV_READ | EV_PERSIST,
+			    stdin_handler, (void *)&data);
+	err = event_add(ev_io, NULL);
 	if(err != 0){
 		perror("event_add");
 		close(soc);
+        event_base_free(base);
 		exit(1);
 	}
 	fprintf(stdout, "> ");
 
-	event_dispatch();
+	event_base_dispatch(base);
 	fprintf(stderr, "END\n");
+    event_base_free(base);
+    freeaddrinfo(info);
 	return 0;
 }
 
